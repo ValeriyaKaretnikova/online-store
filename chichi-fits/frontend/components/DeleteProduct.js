309aa3939462fed6824addeb2f3e7c1fe7399dc6
Valeriya Karetnikova <valeriya.karetnikova@gmail.com>
2023-04-12 08:57:56 -0600

    update cache, pagination

--- chichi-fits/frontend/components/DeleteProduct.js
+++ chichi-fits/frontend/components/DeleteProduct.js
@@ -9,10 +9,14 @@ const DELETE_PRODUCT_MUTATION = gql`
     }
   }
 `;
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
 
 export default function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
     variables: { id },
+    update,
   });
   return (
     <button
@@ -21,8 +25,6 @@ export default function DeleteProduct({ id, children }) {
       onClick={() => {
         // eslint-disable-next-line no-restricted-globals
         if (confirm('Are you sure you want to delete this item?')) {
-          // go ahead
-          console.log('deleting');
           deleteProduct().catch((err) => alert(err.message));
         }
       }}
